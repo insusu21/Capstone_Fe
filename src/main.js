f2d5e21c136e514d135438164f9c7aabbe9291b2
insusu21 <dlstn0201@daum.net>
2025-04-04 14:55:31 +0900

    뷰 설치

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // 라우터 임포트
+import store from './store/store'; // Vuex store 임포트
 
 const app = createApp(App);
 
 app.use(router); // 라우터 추가
+app.use(store); // Vuex 등록
 app.mount('#app'); // HTML의 #app 요소에 마운트
